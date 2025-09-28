8ad38138e3ecb807997a3f4b987b88ee6fbb6ee7
ChristopherLozada2005 <christopher.lozada@tecsup.edu.pe>
2025-09-27 21:50:58 -0500

    Rename the event image URL field to avatar_url

--- src/middleware/cloudinaryUpload.js
+++ src/middleware/cloudinaryUpload.js
@@ -117,7 +117,7 @@ const handleEventImageUpload = (req, res, next) => {
 
     // Si se subió archivo, agregar URL al body
     if (req.file) {
-      req.body.imagen_url = req.file.path;
+      req.body.avatar_url = req.file.path;
       req.uploadedFile = {
         url: req.file.path,
         publicId: req.file.filename
